1a50115af9a675b83e4332c50776fc042ae76ef2
lalo <lalopsb@hotmail.com>
2020-09-26 01:54:31 -0300

    cleaned unnecesary files

--- src/db/repositories/repository.js
+++ src/db/repositories/repository.js
@@ -1,12 +1,7 @@
 import { dynamodbClient } from '../../libs/dynamodb-config';
-import { testDynamodbClient } from '../../../tests/libs/test-dynamodb-config';
 class Repository {
   constructor() {
-    this.client = this.defineStageDynamodbClient();
-  }
-
-  defineStageDynamodbClient() {
-    return process.env.stage === ('dev' || 'prod' || 'review') ? dynamodbClient : testDynamodbClient;
+    this.client = dynamodbClient;
   }
 }
 
--- src/libs/dynamodb-config.js
+++ src/libs/dynamodb-config.js
@@ -1,11 +1,5 @@
 const AWS = require("aws-sdk");
 
-if (['dev','prod','review'].includes(process.env.stage)) {
-  AWS.config.update({
-    region: "us-east-1",
-  });
-}
-
 export const dynamodb = new AWS.DynamoDB();
 
 export const dynamodbClient = new AWS.DynamoDB.DocumentClient({ convertEmptyValues: true });
